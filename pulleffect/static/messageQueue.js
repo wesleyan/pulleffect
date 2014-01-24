e4b0b1ab94381c3bc5a1fd0237b75dfb9fa7c0c0
jlashner <jlashner@gmail.com>
2014-01-24 15:21:00 -0500

    Added icons to represent devices

--- pulleffect/static/messageQueue.js
+++ pulleffect/static/messageQueue.js
@@ -2,16 +2,21 @@
 var sampleMessages = [
 	{device: "name1", device_type: "mac", location:"SCIE 127", severity: 1, 
 	 title: "Turning off", description: "Turning off", time: "12:00"},
-	 {device: "name2", device_type: "mac", location:"SCIE 127", severity: 3, 
+	 {device: "name2", device_type: "pc", location:"SCIE 127", severity: 2, 
 	 title: "Turning off", description: "Turning off", time: "12:00"},
-	 {device: "name1", device_type: "mac", location:"SCIE 127", severity: 5, 
+	 {device: "name3", device_type: "roomtrol", location:"SCIE 127", severity: 3, 
+	 title: "Turning off", description: "Turning off", time: "12:00"},
+	 {device: "name4", device_type: "printer", location:"SCIE 127", severity: 4,
+	 title: "Turning off", description: "Turning off", time: "12:00"},
+	 {device: "name5", device_type: "roomtrol", location:"SCIE 127", severity: 5, 
 	 title: "Turning off", description: "Turning off", time: "12:00"}
 ];
 
 var displayMessages = function(messages){
 
-	var panel = $("<div />");
-	var table = $("<table />");
+	var panel = $("<div />").width("300px");
+	panel.addClass("panel panel-default")
+	var table = $("<table />").addClass("table table-striped table-condensed");
 
 	messages = sampleMessages;
 
@@ -28,7 +33,17 @@ var displayMessages = function(messages){
 
 
 		row.append($('<td />').text(messages[i].device))
-		row.append($('<td />').text(messages[i].device_type));
+
+
+		switch (messages[i].device_type){
+			case "mac": var icon = "fa fa-apple"; break;
+			case "pc": var icon = "fa fa-windows"; break;
+			case "printer": var icon = "fa fa-print"; break;
+			case "roomtrol": var icon = "fa fa-flash"; break;
+			default: var icon = "";
+		}
+		
+		row.append($('<td />').append($('<span />').addClass(icon)));
 		row.append($('<td />').text(messages[i].location));
 		row.append($('<td />').text(messages[i].title));
 		row.append($('<td />').text(messages[i].description));
