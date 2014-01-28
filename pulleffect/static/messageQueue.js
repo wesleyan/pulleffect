60334cbfe4606eef549a3a2b4d9d735a441a56e4
jlashner <jlashner@gmail.com>
2014-01-28 16:46:52 -0500

    Starting animation for messageQueue

--- pulleffect/static/messageQueue.js
+++ pulleffect/static/messageQueue.js
@@ -12,48 +12,87 @@ var sampleMessages = [
 	 title: "Turning off", description: "Turning off", time: "12:00"}
 ];
 
+
+
+var formRow = function(message, cb){
+	var row = $('<tr />');
+	row.css({fontSize: "11px"});
+
+	if (message.severity == 3 || message.severity == 4)
+		row.addClass("text-warning");
+	else if (message.severity == 5)
+		row.addClass("text-danger");
+
+	row.append($('<td />').text(message.device))
+
+
+	switch (message.device_type){
+		case "mac": var icon = "fa fa-apple"; break;
+		case "pc": var icon = "fa fa-windows"; break;
+		case "printer": var icon = "fa fa-print"; break;
+		case "roomtrol": var icon = "fa fa-flash"; break;
+		default: var icon = "";
+	}
+	
+	row.append($('<td />').append($('<span />').addClass(icon)));
+	row.append($('<td />').text(message.location));
+	row.append($('<td />').text(message.title));
+	row.append($('<td />').text(message.description));
+	row.append($('<td />').text(message.time));
+
+
+
+	if (typeof cb === 'function'){
+		cb(row);
+	}
+}
+
+var addRow = function(){
+
+	var lastRow = $("table tr:last-child");
+
+	lastRow.children('td').animate({padding: 0})
+		.wrapInner('<div />')
+		.children()
+		.slideUp(200, function(){
+			$(this).parent().parent().remove();
+		});
+
+
+	var newRow = $("<tr />");
+	newRow.append($("<td>HERE</td>"));
+	newRow.children().animate({padding:0}).wrapInner("<div />");
+	newRow.children().children().css({display: 'none'});//.text("HERE");
+	$("table").prepend(newRow);
+	newRow.children().children().slideDown(300, function(){
+
+	});
+
+
+}
+
 var displayMessages = function(messages){
 
 	var panel = $("<div />").width("300px");
 	panel.addClass("panel panel-default")
-	var table = $("<table />").addClass("table table-striped table-condensed");
+	var table = $("<table />").addClass("table table-condensed");
 
 	messages = sampleMessages;
 
 	var maxMessages = 5
 
 	for(var i = 0; i < messages.length && i < maxMessages; i++){
-		var row = $('<tr />');
 
-		if (messages[i].severity == 3 || messages[i].severity == 4)
-			row.addClass("text-warning");
-		else if (messages[i].severity == 5)
-			row.addClass("text-danger");
+		formRow(messages[i], function(row){
+			table.append(row);
+		});
 
-
-
-		row.append($('<td />').text(messages[i].device))
-
-
-		switch (messages[i].device_type){
-			case "mac": var icon = "fa fa-apple"; break;
-			case "pc": var icon = "fa fa-windows"; break;
-			case "printer": var icon = "fa fa-print"; break;
-			case "roomtrol": var icon = "fa fa-flash"; break;
-			default: var icon = "";
-		}
-		
-		row.append($('<td />').append($('<span />').addClass(icon)));
-		row.append($('<td />').text(messages[i].location));
-		row.append($('<td />').text(messages[i].title));
-		row.append($('<td />').text(messages[i].description));
-		row.append($('<td />').text(messages[i].time));
-
-
-		table.append(row);
 	}
 
-
+	panel.height('200px');
 	panel.append(table);
+	bt = $("<button />").attr("type", "button").addClass("btn btn-success").text("Update").click(addRow);
+	bt.css({bottom: "0px"});
+	panel.append(bt);
 	$('.Widgets').append(panel);
 }
