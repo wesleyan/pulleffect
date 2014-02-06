3f43df4515da5d2f0a67b48c386bab2133d163a6
jlashner <jlashner@gmail.com>
2014-02-05 19:41:54 -0500

    Working on special events list

--- pulleffect/static/specialEvents/specialEvents.js
+++ pulleffect/static/specialEvents/specialEvents.js
@@ -2,10 +2,24 @@
 var loadEventTable = function(event, cb){
 
 	var eventItem = $('<li />').addClass('list-group-item').css('font-size', '12px');
-	eventItem.append($('<div />').html(event.title).css('border-bottom', '1px solid #EEE'));
-	var timeStr =  moment(event.start).format('hh:mm A') + '-' + moment(event.end).format('hh:mm A');
-	eventItem.append($('<div />').html(timeStr));
-	
+	eventItem.append($('<div />').html(event.title).css('border-bottom', '1px solid #aaa'));
+
+	var timeDiv = $('<div />');
+	timeDiv.append('<b>' + moment(event.start).format('hh:mm A') + '</b> ');
+	timeDiv.append('<i>' + moment(event.eventStart).format('hh:mm A') + '</i>');
+	timeDiv.append(' <i class="fa fa-long-arrow-right"></i> ');
+	timeDiv.append('<i>' + moment(event.eventEnd).format('hh:mm A') + '</i> ');
+	timeDiv.append('<b>' + moment(event.end).format('hh:mm A') + '</b>');
+
+	eventItem.append(timeDiv);
+
+	if (event.cancelled){
+		eventItem.css('background-color', '#FFABAB');
+	}
+
+	eventItem.hover(function(){$(this).css('background-color', '#eee');}, 
+					function(){$(this).css('background-color', '#fff')});
+		
 
 	if (typeof cb === 'function')
 		cb(eventItem);
@@ -26,7 +40,7 @@ var renderSpecialEventsWidget = function(){
 	var eventsList = $("<ul />").addClass("eventsList").addClass('list-group ')
 	panel.append(eventsList);
 
-	$.getJSON('http://ims-dev.wesleyan.edu:8080/api/events?minutes=5000')
+	$.getJSON('http://ims-dev.wesleyan.edu:8080/api/events?minutes=20000')
 		.success(renderSpecialEvents)
 		.error(function(err){
         	console.log(err);
