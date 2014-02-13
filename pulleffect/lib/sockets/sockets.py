67b4c238d5b2ab19c057f64f753601e57fab0f2d
Erik Islo <eislo@wesleyan.edu>
2014-02-13 12:07:25 -0500

    starting commit in branch. sockets file created.

---
+++ pulleffect/lib/sockets/sockets.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
