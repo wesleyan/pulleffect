91acbdb1620eaf787ff117d0df2075ea938c8824
Arthur <artburkart@gmail.com>
2014-05-01 14:32:24 -0400

    almost done with timeclock entries\!

--- pulleffect/lib/timeclock/timeclock.py
+++ pulleffect/lib/timeclock/timeclock.py
@@ -3,36 +3,41 @@ from flask import jsonify
 from flask import url_for
 from datetime import datetime, timedelta
 from pulleffect.config.env import config
-from pulleffect.lib.timeclock.timeclock_models import TimeclockEntry
+from pulleffect.lib.timeclock.timeclock_models import TimeclockEntries
 from pulleffect.lib.utilities import signin_required
+from pulleffect.lib.utilities import sqla_session
 
 timeclock = Blueprint('timeclock', __name__, template_folder='templates')
 
-timeclock_entries = Table('NED_SHIFT')
+# Get table with timeclock entries
+timeclock_entries = sqla_session.query(TimeclockEntries).all()
+
 
 @timeclock.route('')
 @signin_required
 def index():
-	# TimeclockEntry.query.filter((TimeclockEntry.time_out == None)
-	# 	|| (TimeclockEntry.time_in > datetime.today() - timedelta(hours=5)))
+    for entry in timeclock_entries:
 
-	# Grab the ten most recently clocked in workers
-	timeclock_entries = TimeclockEntry.query.order_by(TimeclockEntry.time_in).limit(10)
+    # TimeclockEntry.query.filter((TimeclockEntry.time_out == None)
+    # 	|| (TimeclockEntry.time_in > datetime.today() - timedelta(hours=5)))
 
-	for timeclock_entry in timeclock_entries:
-		print timeclock_entry
-	# # Open connection to database
-	# connection = wes_timeclock_engine.begin()
+    # Grab the ten most recently clocked in workers
+    timeclock_entries = TimeclockEntry.query.order_by(TimeclockEntry.time_in).limit(10)
 
-	# # Get the most recently clocked in employee
-	# employees = connection.execute("SELECT * FROM "
-	# 	"(SELECT USERNAME, ID, TIME_IN, TIME_OUT FROM ACLC_USDAN.NED_SHIFT ORDER BY TIME_IN DESC) "
-	# 	"WHERE ROWNUM = 1")
+    for timeclock_entry in timeclock_entries:
+        print timeclock_entry
+    # # Open connection to database
+    # connection = wes_timeclock_engine.begin()
 
-	# for employee in employees:
-	# 	print "username", employee["USERNAME"]
+    # # Get the most recently clocked in employee
+    # employees = connection.execute("SELECT * FROM "
+    # 	"(SELECT USERNAME, ID, TIME_IN, TIME_OUT FROM ACLC_USDAN.NED_SHIFT ORDER BY TIME_IN DESC) "
+    # 	"WHERE ROWNUM = 1")
 
-	# connection.close()
+    # for employee in employees:
+    # 	print "username", employee["USERNAME"]
+
+    # connection.close()
 
 
 
--- pulleffect/lib/utilities.py
+++ pulleffect/lib/utilities.py
@@ -2,18 +2,21 @@ from pymongo import MongoClient
 from functools import wraps
 from flask import session
 from flask import render_template
-# from pulleffect.config.env import config
-# from sqlalchemy import create_engine
+from pulleffect.config.env import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 # Mongo database connection
 client = MongoClient('localhost', 27017)
 mongo_connection = client.pulleffect
 
 # Wes timeclock database connection
-# wes_timeclock_engine = create_engine('oracle://{0}:{1}@{2}'.format(
-#     config['wes_timeclock_username'],
-#     config['wes_timeclock_password'],
-#     config['wes_timeclock_connection_string']))
+wes_timeclock_engine = create_engine('oracle://{0}:{1}@{2}'.format(
+    config['wes_timeclock_username'],
+    config['wes_timeclock_password'],
+    config['wes_timeclock_connection_string']), echo=True)
+
+sqla_session = sessionmaker(bind=wes_timeclock_engine)
 
 # This dictionary contains all the database names
 # that we use, so if we ever name them something different,
