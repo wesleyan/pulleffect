7180f80a758612bc70cf3125a3457c81ac454574
Arthur <artburkart@gmail.com>
2014-05-01 13:45:26 -0400

    Removed some oracle deps so Jack and keep working

--- pulleffect/lib/utilities.py
+++ pulleffect/lib/utilities.py
@@ -2,35 +2,24 @@ from pymongo import MongoClient
 from functools import wraps
 from flask import session
 from flask import render_template
-from pulleffect.config.env import config
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+# from pulleffect.config.env import config
+# from sqlalchemy import create_engine
 
 # Mongo database connection
 client = MongoClient('localhost', 27017)
 mongo_connection = client.pulleffect
 
 # Wes timeclock database connection
-wes_timeclock_engine = create_engine('oracle://{0}:{1}@{2}'.format(
-    config['wes_timeclock_username'],
-    config['wes_timeclock_password'],
-    config['wes_timeclock_connection_string']))
-
-Session = sessionmaker(bind=wes_timeclock_engine)
-session = Session()
+# wes_timeclock_engine = create_engine('oracle://{0}:{1}@{2}'.format(
+#     config['wes_timeclock_username'],
+#     config['wes_timeclock_password'],
+#     config['wes_timeclock_connection_string']))
 
 # This dictionary contains all the database names
 # that we use, so if we ever name them something different,
 # we can just change the name in one spot
 db_names = {
     "wes_timeclock": "wes_timeclock"
-}
-
-# This is the object we use at the global level for utilities
-util = {
-    "db_names": db_names,
-    "session": session,
-    "mongo_connection": mongo_connection
 }
 
 
