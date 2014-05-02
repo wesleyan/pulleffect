f4bac7756ec553cade6ab7955782ce8be03eca95
Arthur <artburkart@gmail.com>
2014-05-02 14:05:38 -0400

    getting somewhere with timeclock shit

--- pulleffect/lib/timeclock/timeclock_entry.py
+++ pulleffect/lib/timeclock/timeclock_entry.py
@@ -4,6 +4,9 @@ from pulleffect.lib.databases import db
 class TimeclockEntry(db.Model):
     __tablename__ = 'NED_SHIFT'
 
+    # Id
+    id = db.Column('ID', db.Integer, primary_key=True)
+
     # Username
     username = db.Column('USERNAME', db.String(80))
 
