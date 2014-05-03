f2c6ccb03c75f0608ce4166f10ae9d3989b08d83
Arthur <artburkart@gmail.com>
2014-05-02 23:11:25 -0400

    redoing everything cuz everything sucks

--- pulleffect/lib/timeclock/timeclock.py
+++ pulleffect/lib/timeclock/timeclock.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask import jsonify
 from pulleffect.lib.utilities import signin_required
-from pulleffect.lib.timeclock.timeclock_entry import TimeclockEntry
+from pulleffect.lib.utilities import wes_timeclock_pool
+
 
 timeclock = Blueprint('timeclock', __name__, template_folder='templates')
 
@@ -9,19 +10,28 @@ timeclock = Blueprint('timeclock', __name__, template_folder='templates')
 @timeclock.route('')
 @signin_required
 def index():
-    timeclock_entry = TimeclockEntry.query.filter_by(
-        username='aburkart').first()
-    print timeclock_entry.time_in
-    return jsonify(timeclock_entry)
+    try:
+        cursor = wes_timeclock_pool.cursor()
+        cursor.execute("SELECT * FROM (SELECT USERNAME, ID, TIME_IN,"
+                       " TIME_OUT FROM ACLC_USDAN.NED_SHIFT ORDER BY "
+                       "TIME_IN DESC) WHERE ROWNUM = 1")
+        timeclock_entries = []
+        for row in cursor.fetchall():
+            timeclock_entries.append(row)
+        return jsonify(timeclock_entries)
+
+    # timeclock_entry = TimeclockEntry.query.filter_by(
+    #     username='aburkart').first()
+    # print timeclock_entry.time_in
+    # return jsonify(timeclock_entry)
 
     # # Open connection to database
     # connection = wes_timeclock_engine.begin()
 
     # # Get the most recently clocked in employee
-    # employees = connection.execute("SELECT * FROM "
-    # 	"(SELECT USERNAME, ID, TIME_IN, TIME_OUT FROM ACLC_USDAN.NED_SHIFT"
-    # " ORDER BY TIME_IN DESC) "
-    # 	"WHERE ROWNUM = 1")
+    # employees = connection.execute("SELECT * FROM (SELECT USERNAME, ID, "
+    # "TIME_IN, TIME_OUT FROM ACLC_USDAN.NED_SHIFT ORDER BY TIME_IN DESC) "
+    # " WHERE ROWNUM = 1")
 
     # for employee in employees:
     # 	print "username", employee["USERNAME"]
--- pulleffect/lib/utilities.py
+++ pulleffect/lib/utilities.py
@@ -3,12 +3,29 @@ from functools import wraps
 from flask import session
 from flask import render_template
 from pulleffect.config.env import config
+import cx_Oracle
+
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker
 
 # Mongo database connection
 client = MongoClient('localhost', 27017)
 mongo_connection = client.pulleffect
+
+wes_timeclock_username = config['wes_timeclock_username']
+wes_timeclock_password = config['wes_timeclock_password']
+
+# Construct oracle dsn
+wes_timeclock_dsn = cx_Oracle.makedsn(host='curltest.db.wesleyan.edu',
+                                      port=2111,
+                                      service_name='CURLTEST.WESLEYAN.EDU')
+
+# Construct oracle database connection pool
+wes_timeclock_pool = cx_Oracle.SessionPool(wes_timeclock_username,
+                                           wes_timeclock_password,
+                                           wes_timeclock_dsn, 1, 4, 1)
+# Timeout for connection
+# wes_timeclock_pool.timeout = 50
 
 # Wes timeclock database connection
 # wes_timeclock_engine = create_engine('oracle://{0}:{1}@{2}'.format(
