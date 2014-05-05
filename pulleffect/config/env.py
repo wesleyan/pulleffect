d6c45c8836ced6a2d8b2d7c9a62275ad034a0898
Arthur Burkart <artburkart@gmail.com>
2014-05-05 17:23:40 -0400

    updated env.py to 'is_beta' etc
    changed in development ages ago and now it's causing troubles when I push to dev server

--- pulleffect/config/env.py
+++ pulleffect/config/env.py
@@ -4,12 +4,12 @@ import json
 	In order to develop on your machine, set beta and production to False.
 	Make sure you do not commit any changes you make to this file, otherwise you will break the website.
 """
-production = False
-beta = True
+is_production = False
+is_beta = True
 
 # Read configurations
-config = (open('pulleffect/config/prod_config.json') if production 
-	else (open('pulleffect/config/beta_config.json') if beta else open('pulleffect/config/dev_config.json')))
+config = (open('pulleffect/config/prod_config.json') if is_production 
+	else (open('pulleffect/config/beta_config.json') if is_beta else open('pulleffect/config/dev_config.json')))
 
 # Export configurations
 config = json.load(config)
